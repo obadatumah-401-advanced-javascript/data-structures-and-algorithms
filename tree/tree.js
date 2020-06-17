748e6f33184cd791b28a88ddfcee6a093672f705
Tumaho <obadatumah19@gmail.com>
2020-06-17 14:13:53 +0300

    odd

--- tree/tree.js
+++ tree/tree.js
@@ -69,14 +69,23 @@ class BinaryTree {
   findMaximumValue() {
     var max = 0;
     const traverse = (node) => {
-      if(max < node.value) max = node.value;
+      if (max < node.value) max = node.value;
       if (node.left) traverse(node.left);
       if (node.right) traverse(node.right);
     }
     traverse(this.root);
     return max;
+  }
 
-
+  oddSum() {
+    var sum = 0;
+    const traverse = (node) => {
+      if (node.value %2 !==0) sum += node.value;
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return sum;
   }
 
 }
